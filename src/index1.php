4f07a6a87a5d2e1f273dee4d0dc98dc04f85323f
belocer <belocerkovecden@gmail.com>
2020-06-29 19:01:15 +0300

    first commit

---
+++ src/index1.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Реклама в метро</title>
+    <link rel="stylesheet" href="css/main.css?ver=<?= rand(10000, 99999)?>">
+</head>
+<body>
+
+<div class="wrapper_full">
+    <section class="first">
+        <div class="outer br_violet">
+            <div class="logo__wrap">
+                <a href="/" class="logo__link"></a>
+            </div>
+
+            <div class="wrap_description_violet">
+                <h1 class="main_header">
+                    <span class="main_header__fill">Реклама</span>
+                    <span class="main_header__empty">в метро</span>
+                </h1>
+
+                <ul class="list_statistics">
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Вовлечённость</span>
+                        <span class="list_statistics__num">9 / 10</span>
+                    </li>
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Средний охват</span>
+                        <span class="list_statistics__num">2 / 10</span>
+                    </li>
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Затраты</span>
+                        <span class="list_statistics__num">1 / 10</span>
+                    </li>
+                </ul>
+
+                <a href="#" class="link_more_details">
+                    <span class="link_more_details__text">Подробнее</span>
+                    <span class="link_more_details__icon">&#8594;</span>
+                </a>
+            </div>
+
+            <!-- Бегущая строка - Наружная реклама -->
+            <div class="wrap_ticker_violet">
+                <div class="ticker_violet">
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                    <span class="ticker__text_violet">Реклама в метро&nbsp;</span>&nbsp;
+                </div>
+            </div>
+        </div>
+        <div class="nav-block">
+            <div class="nav-block__head">
+                <a href="#" class="run_project color_violet">Запустить проект</a>
+                <a href="#" class="sandwich"> </a>
+            </div>
+
+            <div class="integer">01 / 09</div>
+
+            <div class="nav-block__bottom">
+                <a href="#" class="go_over">К разделу о компании</a>
+                <a href="#" class="arrow_down">&#8595;</a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="js/main.min.js"></script>
+
+</body>
+</html>
---
+++ src/index2.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Реклама на транспорте</title>
+    <link rel="stylesheet" href="css/main.css?ver=<?= rand(10000, 99999)?>">
+</head>
+<body>
+
+<div class="wrapper_full">
+    <section class="first">
+        <div class="outer br_green">
+            <div class="logo__wrap">
+                <a href="/" class="logo__link"></a>
+            </div>
+
+            <div class="wrap_description_green">
+                <h1 class="main_header">
+                    <span class="main_header__fill">Реклама</span>
+                    <span class="main_header__empty">на транспорте</span>
+                </h1>
+
+                <ul class="list_statistics">
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Вовлечённость</span>
+                        <span class="list_statistics__num">9 / 10</span>
+                    </li>
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Средний охват</span>
+                        <span class="list_statistics__num">2 / 10</span>
+                    </li>
+                    <li class="list_statistics__item">
+                        <span class="list_statistics__text">Затраты</span>
+                        <span class="list_statistics__num">1 / 10</span>
+                    </li>
+                </ul>
+
+                <a href="#" class="link_more_details">
+                    <span class="link_more_details__text">Подробнее</span>
+                    <span class="link_more_details__icon">&#8594;</span>
+                </a>
+            </div>
+
+            <!-- Бегущая строка - Наружная реклама -->
+            <div class="wrap_ticker_green">
+                <div class="ticker_green">
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                    <span class="ticker__text_green">Реклама на транспорте&nbsp;</span>&nbsp;
+                </div>
+            </div>
+        </div>
+        <div class="nav-block">
+            <div class="nav-block__head">
+                <a href="#" class="run_project color_green">Запустить проект</a>
+                <a href="#" class="sandwich"> </a>
+            </div>
+
+            <div class="integer">01 / 09</div>
+
+            <div class="nav-block__bottom">
+                <a href="#" class="go_over">К разделу о компании</a>
+                <a href="#" class="arrow_down">&#8595;</a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="js/main.min.js"></script>
+
+</body>
+</html>
